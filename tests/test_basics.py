992aaa9c9c937dcbba1a6e00c10c76b344c8cdbb
Thomas Kuryura <thomas.adm@gmail.com>
2021-01-25 19:23:47 +0000

    adding working Pytest base

---
+++ tests/test_basics.py
@@ -0,0 +1,12 @@
+from python_pytest.basics import add
+
+def test_add_can_add_numbers():
+    # given
+    num = 3
+    num2 = 45
+
+    # when
+    result = add(num, num2)
+
+    # then
+    assert result == 48

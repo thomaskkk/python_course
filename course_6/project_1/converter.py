ee97b47ba5eb547754acafeb924c0925b44fba97
Thomas <thomas.kuryura@tembici.com>
2021-01-30 23:37:34 -0300

    Adding converter.py
    It will convert images in the src directory and save into dest directory

---
+++ course_6/project_1/converter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from PIL import Image
+import os
+
+#src_img = "/home/thomas/python_course/course_6/project_1/images/ic_add_location_black_48dp"
+#dest_img = "/home/thomas/python_course/course_6/project_1/test_img/ic_add_location_black_48dp"
+img_format = "JPEG"
+img_rotate = 90
+img_width = 128
+img_height = 128
+
+def converter(src_dir, dest_dir):
+    """Convert image of all images from source dir and saves on destination dir"""
+    for (root, dirs, files) in os.walk(src_dir):
+        for name in files:
+            convert_img_size(os.path.join(root, name), dest_dir + name)
+            convert_img_orientation(dest_dir + name, dest_dir + name)
+
+
+def convert_img_size(src_img, dest_img):
+    """Convert images to the desired width, height and format"""
+    try:
+        im = Image.open(src_img)
+    except:
+        print("Not a valid source image: " + src_img)
+    
+    im.resize((img_width, img_height)).convert('RGB').save(dest_img, img_format)
+
+def convert_img_orientation(src_img, dest_img):
+    """Rotate images and save with the desired format"""
+    try:
+        im = Image.open(src_img)
+    except:
+        print("Not a valid source image: " + src_img)
+    
+    im.rotate(img_rotate).convert('RGB').save(dest_img, img_format)
+
+if __name__ == '__main__':
+    src_dir = "/home/thomas/python_course/course_6/project_1/images/"
+    dest_dir = "/home/thomas/python_course/course_6/project_1/opt/icons/"
+    converter(src_dir, dest_dir)
--- tests/course_6/project_1/test_converter.py
+++ tests/course_6/project_1/test_converter.py
@@ -1,21 +1,35 @@
-from course_6.project_1.convert import convert_img_size, convert_img_format
+from course_6.project_1.converter import convert_img_size, convert_img_orientation
 
-import PIL, os
+from PIL import Image
+import os
 
 src_img = "/home/thomas/python_course/course_6/project_1/images/ic_add_location_black_48dp"
 dest_img = "/home/thomas/python_course/course_6/project_1/test_img/ic_add_location_black_48dp"
 
 def test_img_size():
     convert_img_size(src_img, dest_img)
+    
+    #open image to check size
     im = Image.open(dest_img)
     width, height = im.size
-    assert width == "128" and height == "128"
+    assert width == 128 and height == 128
+    
+    #delete file after assertion
+    if os.path.exists(dest_img):
+        os.remove(dest_img)
+
 
 def test_img_format():
-    convert_img_format(src_img, dest_img)
+    convert_img_orientation(src_img, dest_img)
+    
+    #open image to check format
     im = Image.open(dest_img)
     img_format = im.format
-    assert img_format == "jpeg"
+    assert img_format == "JPEG"
 
-#def test_img_orientation():
+    #delete file after assertion
+    if os.path.exists(dest_img):
+        os.remove(dest_img)
+
+#def test_not_img():
 #    pass
